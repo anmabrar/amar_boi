3e9d79478729d5ac2619b8e52400de95fc770d44
anmabrar <17101013@uap-bd.edu>
2024-06-26 08:36:43 +0600

    add README.md

--- amar_boi/urls.py
+++ amar_boi/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for amar_boi project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
 
@@ -26,7 +10,7 @@ schema_view = get_schema_view(
       title="Amar Boi",
       default_version='v1',
       description="""
-        Amar Boi is an online bookshop API developed using Django Rest Framework (DRF). It integrates djoser for user management,\
+        Amar Boi is an online bookshop API developed using Django Rest Framework (DRF). It integrates user management,\
         JWT for secure authentication, and django_filters for advanced filtering capabilities. Utilizing nested routers, it organizes \
         various endpoints for seamless navigation. The API supports comprehensive models, including Category, Author, Publication, Book, \
         Order, OrderItem, Customer, Review, Cart, and CartItem. This setup ensures a robust and scalable system, facilitating efficient \
@@ -54,6 +38,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('amar-boi/', include('core.urls')),
+    path('', include('core.urls'), name='user'),
     path('', include('store.urls')),
 ]
